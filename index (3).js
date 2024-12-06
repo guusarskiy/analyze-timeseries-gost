919ad72a3addeed8342671265252325cb0cc1f31
guusarskiy <kosars2002@yandex.ru>
2024-12-06 14:18:17 +0500

    скрипт и node.js оболочка

---
+++ index (3).js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { PythonShell } from 'python-shell';
+
+const app = express();
+
+const pythonPath = '/'; 
+
+app.use(express.json());
+
+const inputData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let output = '';
+
+app.post('/', (req, res) => {
+  console.log('POST получен*\n');
+});
+
+let pyshell = new PythonShell('dist_data_info.py', {
+  pythonPath: pythonPath,
+  args: [JSON.stringify(inputData)], 
+});
+
+pyshell.on('message', function (message) {
+  output += message; 
+});
+
+pyshell.end(function (err) {
+  if (err) {
+    console.error('Ошибка выполнения Python-скрипта:', err.message);
+    return;
+  }
+  console.log(output); 
+});
+
+app.listen(3000, () => {
+  console.log('localhost \n');
+});
